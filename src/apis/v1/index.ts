2f5f8e6fa992093a7942d52053ce736ef29bb4f7
traitimtrongvang <locxuandao260520033@gmail.com>
2022-11-29 09:09:53 +0700

    feat[#7] update 2

--- src/apis/v1/index.ts
+++ src/apis/v1/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import loginRouter from './auth';
 import examsRouter from './exams';
 import { adminMiddleware , authMiddleware } from 'middlewares/auth';
 
 const router = Router();
 
 router.use('/exams', authMiddleware , adminMiddleware , examsRouter)
+router.use('/auth', loginRouter);
 
 export default router;

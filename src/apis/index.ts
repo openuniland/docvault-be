7bffac90450853c0b183c1e3d2cbd2b9ca025a1f
traitimtrongvang <locxuandao260520033@gmail.com>
2022-11-29 09:08:36 +0700

    feat[#7] update

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import healthRouter from './health';
-import v1Routers from './v1';
+import v1Router from './v1';
 
 
 const router = Router();
 import { asyncRouteHandler } from 'middlewares';
 
 router.use('/health', healthRouter);
-router.use('/v1', asyncRouteHandler(v1Routers));
+router.use('/v1', v1Router);
 
 export default router;
+

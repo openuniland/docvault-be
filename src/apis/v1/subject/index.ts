8b91f8be008b6eca0eae01b8b1a1745d08c7a869
traitimtrongvang <locxuandao260520033@gmail.com>
2022-12-08 16:29:19 +0700

    feat[#10] update

--- src/apis/v1/subject/index.ts
+++ src/apis/v1/subject/index.ts
@@ -1,13 +1,20 @@
 import { Router } from 'express';
 
-import { getSubjects, createSubject, updateSubject, deleteSubject } from './controller';
+import * as controller from './controller';
+import { validationMiddleware } from 'middlewares/validation';
+import { APP_CONSTANTS } from 'utils/constants';
 import { asyncRouteHandler } from 'middlewares';
+import { UpdateSubjectDto, SubjectDto } from './dto/SubjectDto';
 
 const router = Router();
 
-router.put('/:id', asyncRouteHandler(updateSubject));
-router.get('/', asyncRouteHandler(getSubjects));
-router.post('/', asyncRouteHandler(createSubject));
-router.delete('/:id', asyncRouteHandler(deleteSubject));
+router.put(
+  '/:id',
+  validationMiddleware(UpdateSubjectDto, APP_CONSTANTS.body),
+  asyncRouteHandler(controller.updateSubject)
+);
+router.get('/', asyncRouteHandler(controller.getSubjects));
+router.post('/', validationMiddleware(SubjectDto, APP_CONSTANTS.body), asyncRouteHandler(controller.createSubject));
+router.delete('/:id', asyncRouteHandler(controller.deleteSubject));
 
 export default router;

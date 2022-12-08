3dca92b9b2a330c3136a89256041ddf4c6c45ce6
traitimtrongvang <locxuandao260520033@gmail.com>
2022-12-09 03:32:40 +0700

    feat[#10] update 2

--- src/apis/v1/subject/index.ts
+++ src/apis/v1/subject/index.ts
@@ -4,17 +4,22 @@ import * as controller from './controller';
 import { validationMiddleware } from 'middlewares/validation';
 import { APP_CONSTANTS } from 'utils/constants';
 import { asyncRouteHandler } from 'middlewares';
-import { UpdateSubjectDto, SubjectDto } from './dto/SubjectDto';
+import { UpdateSubjectDto, SubjectDto, ParamsSubjectDto } from './dto/SubjectDto';
 
 const router = Router();
 
 router.put(
   '/:id',
   validationMiddleware(UpdateSubjectDto, APP_CONSTANTS.body),
+  validationMiddleware(ParamsSubjectDto, APP_CONSTANTS.params),
   asyncRouteHandler(controller.updateSubject)
 );
 router.get('/', asyncRouteHandler(controller.getSubjects));
 router.post('/', validationMiddleware(SubjectDto, APP_CONSTANTS.body), asyncRouteHandler(controller.createSubject));
-router.delete('/:id', asyncRouteHandler(controller.deleteSubject));
+router.delete(
+  '/:id',
+  validationMiddleware(ParamsSubjectDto, APP_CONSTANTS.params),
+  asyncRouteHandler(controller.deleteSubject)
+);
 
 export default router;

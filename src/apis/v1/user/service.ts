f43b67bb57f66a3101f0ee56b5e06843e566f35c
dunggnguyennn <91179133+DungNguyen2003@users.noreply.github.com>
2022-12-12 14:19:51 +0700

    Feat/#6 (#36)
    * feat/#4
    
    * fix: [#4]
    
    * fix: #4
    
    * fix: [#4] refactor code, impl upsert for create user
    
    * feat/#6
    
    * fix: router delete
    
    Co-authored-by: nc-minh <nguyencongminh.official@gmail.com>

--- src/apis/v1/user/service.ts
+++ src/apis/v1/user/service.ts
@@ -1,7 +1,8 @@
 import { ErrorCodes, HttpException } from 'exceptions';
 import UserModel from 'models/schema/User';
 import { logger } from 'utils/logger';
-import { UserDto } from './dto/UserDto';
+
+import { UserDto, UpdateUserDto } from './dto/UserDto';
 
 export const createUser = async function (input: UserDto) {
   try {
@@ -38,3 +39,34 @@ export const getUsers = async function () {
     throw new HttpException(400, ErrorCodes.BAD_REQUEST.MESSAGE, ErrorCodes.BAD_REQUEST.CODE);
   }
 };
+
+export const updateUser = async function (input: UpdateUserDto, id: string) {
+  try {
+    const users = await UserModel.findOneAndUpdate(
+      { _id: id },
+      {
+        fullname: input.fullname,
+        roles: input.role,
+        is_blocked: input.is_blocked,
+      }
+    );
+    logger.info(`Update for user have id: ${id} successfully`);
+    return users;
+  } catch (error) {
+    logger.error(`Error while update user: ${error}`);
+    throw new HttpException(400, ErrorCodes.BAD_REQUEST.MESSAGE, ErrorCodes.BAD_REQUEST.CODE);
+  }
+};
+
+export const deleteUser = async function (id: string) {
+  try {
+    const users = await UserModel.findOneAndDelete({ _id: id });
+    logger.info(`Delete user have id: ${id} successfully`);
+
+    return users;
+  } catch (error) {
+    logger.error(`Error while delete user: ${error}`);
+    throw new HttpException(400, ErrorCodes.BAD_REQUEST.MESSAGE, ErrorCodes.BAD_REQUEST.CODE);
+  }
+};
+

9b0b66e029c50ba6eeac5a733bf939184c1312b4
traitimtrongvang <locxuandao260520033@gmail.com>
2022-12-15 20:51:27 +0700

    feat[#12]

---
+++ src/apis/v1/answer/index.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import * as controller from './controller';
+import { validationMiddleware } from 'middlewares/validation';
+import { APP_CONSTANTS } from 'utils/constants';
+import { asyncRouteHandler } from 'middlewares';
+import { AnswerDto, UpdateAnswerDto, ParamsAnswerDto } from './dto/AnswerDto';
+
+const router = Router();
+
+router.get('/', asyncRouteHandler(controller.getAnswer));
+router.post('/', validationMiddleware(AnswerDto, APP_CONSTANTS.body), asyncRouteHandler(controller.createAnswer));
+router.put(
+  '/:id',
+  validationMiddleware(UpdateAnswerDto, APP_CONSTANTS.body),
+  validationMiddleware(ParamsAnswerDto, APP_CONSTANTS.params),
+  asyncRouteHandler(controller.updateAnswer)
+);
+router.delete(
+  '/:id',
+  validationMiddleware(ParamsAnswerDto, APP_CONSTANTS.params),
+  asyncRouteHandler(controller.deleteAnswer)
+);
+
+export default router;

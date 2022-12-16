0a810f4b74c82d6ed9440c9b9477dca9e8c02bee
Loc Xuan Dao <102164071+traitimtrongvang@users.noreply.github.com>
2022-12-16 15:49:46 +0700

    feat[#12] (#38)
    * feat[#12]
    
    * feat[#12] update

--- src/apis/v1/index.ts
+++ src/apis/v1/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import authRouter from './auth';
 import userRouter from './user';
 import subjectRouter from './subject';
+<<<<<<< HEAD
 import questionRouter from './questions';
 import answerRouter from './answer';
 import documentRouter from './documents';
@@ -10,12 +11,16 @@ import examRouter from './exam';
 import userExamRouter from './userExam';
 import userAnswerRouter from './userAnswer';
 import adminRouter from './admin';
+=======
+import answerRouter from './answer';
+>>>>>>> 651978a (feat[#12] (#38))
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
 router.use('/subjects', subjectRouter);
+<<<<<<< HEAD
 router.use('/questions', questionRouter);
 router.use('/answers', answerRouter);
 router.use('/documents', documentRouter);
@@ -23,5 +28,8 @@ router.use('/exams', examRouter);
 router.use('/user-exams', userExamRouter);
 router.use('/user-answers', userAnswerRouter);
 router.use('/administrator', adminRouter);
+=======
+router.use('/answers', answerRouter);
+>>>>>>> 651978a (feat[#12] (#38))
 
 export default router;

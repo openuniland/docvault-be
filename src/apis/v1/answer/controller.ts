8797be62a72ab4bd35b0732a974e0decaeaf0253
Loc Xuan Dao <102164071+traitimtrongvang@users.noreply.github.com>
2022-12-16 15:49:46 +0700

    feat[#12] (#38)
    * feat[#12]
    
    * feat[#12] update

---
+++ src/apis/v1/answer/controller.ts
@@ -0,0 +1,34 @@
+import { Response } from 'express';
+
+import * as service from './service';
+import fmt from 'utils/formatter';
+import RequestWithUser from 'utils/rest/request';
+import { AnswerDto, ParamsAnswerDto, UpdateAnswerDto } from './dto/AnswerDto';
+
+export const getAnswers = async (req: RequestWithUser, res: Response) => {
+  const result = await service.getAnswers();
+
+  res.send(fmt.formatResponse(result, Date.now() - req.startTime, 'OK'));
+};
+
+export const createAnswer = async (req: RequestWithUser, res: Response) => {
+  const input: AnswerDto = req.body;
+  const result = await service.createAnswer(input);
+
+  res.send(fmt.formatResponse(result, Date.now() - req.startTime, 'OK'));
+};
+
+export const updateAnswer = async (req: RequestWithUser, res: Response) => {
+  const params: ParamsAnswerDto = req.params;
+  const input: UpdateAnswerDto = req.body;
+  const result = await service.updateAnswer(input, params.id);
+
+  res.send(fmt.formatResponse(result, Date.now() - req.startTime, 'OK'));
+};
+
+export const deleteAnswer = async (req: RequestWithUser, res: Response) => {
+  const params: ParamsAnswerDto = req.params;
+  const result = await service.deleteAnswer(params.id);
+
+  res.send(fmt.formatResponse(result, Date.now() - req.startTime, 'OK'));
+};
--- src/apis/v1/index.ts
+++ src/apis/v1/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import authRouter from './auth';
 import userRouter from './user';
 import subjectRouter from './subject';
+import answerRouter from './answer';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
 router.use('/subjects', subjectRouter);
+router.use('/answers', answerRouter);
 
 export default router;

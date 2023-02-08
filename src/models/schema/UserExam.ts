a895c0b295bd18d70f1a1db11663b2844fcb1189
Nguyen Cong Minh <59333368+nc-minh@users.noreply.github.com>
2023-02-08 10:10:43 +0700

    feat: [#79] (#83)

--- src/models/schema/UserExam.ts
+++ src/models/schema/UserExam.ts
@@ -17,6 +17,7 @@ const UserExamSchema = new Schema<UserExam>(
     user_answer_id: { type: Schema.Types.ObjectId, ref: MODELS.user_answer, required: true },
     duration: { type: Schema.Types.Number, required: true },
     score: { type: Schema.Types.Number, default: 0 },
+    semester: { type: String, required: true },
     is_deleted: { type: Boolean, default: false },
     is_completed: { type: Boolean, default: false },
   },

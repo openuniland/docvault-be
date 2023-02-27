dec6c5a4c867ac4c72ad90585a8bb2275da88078
nc-minh <nguyencongminh.official@gmail.com>
2023-02-27 14:44:03 +0700

    fix: add subject into userExam

--- src/apis/v1/exam/index.ts
+++ src/apis/v1/exam/index.ts
@@ -9,42 +9,28 @@ import { ExamDto, ParamsExamDto, QueryExamDto, UpdateExamDto } from './dto/ExamD
 const router = Router();
 
 router.get('/', authMiddleware, asyncRouteHandler(controller.getExams));
+
 router.get(
   '/subjectname',
   authMiddleware,
   validationMiddleware(QueryExamDto, APP_CONSTANTS.query),
   asyncRouteHandler(controller.getExamBySubject)
 );
+
 router.get(
   '/:id',
-<<<<<<< HEAD
-<<<<<<< HEAD
   authMiddleware,
-=======
->>>>>>> 4920e19 (feat/#48 (#58))
-=======
-  authMiddleware,
->>>>>>> fab65e0 (feat: [#5] (#67))
   validationMiddleware(ParamsExamDto, APP_CONSTANTS.params),
   asyncRouteHandler(controller.getExamById)
 );
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> fab65e0 (feat: [#5] (#67))
 router.post(
   '/',
   authMiddleware,
   validationMiddleware(ExamDto, APP_CONSTANTS.body),
   asyncRouteHandler(controller.createExam)
 );
-<<<<<<< HEAD
-=======
-router.post('/', validationMiddleware(ExamDto, APP_CONSTANTS.body), asyncRouteHandler(controller.createExam));
->>>>>>> 4920e19 (feat/#48 (#58))
-=======
->>>>>>> fab65e0 (feat: [#5] (#67))
+
 router.put(
   '/:id',
   authMiddleware,

fbd5db40400e7ca880e25e628750e7b05dc243e8
Nguyen Cong Minh <59333368+nc-minh@users.noreply.github.com>
2023-03-10 17:05:29 +0700

    Dev - update auth api for the admin (#122)
    * fix: remove unused validate
    
    * fix: add subject into userExam
    
    * feat: create api get all documents by subjectId
    
    * fix: remove validation for author field in DocumentDto
    
    * fix: update response of verifyAccessToken
    
    * fix: update response of verifyAccessToken
    
    * fix: add userInfo into response refreshToken
    
    * fix: add subject to response of getDocumentBySubject api
    
    * feat: create api get a subject by id
    
    * feat: create unique for subject_name
    
    * fix: return code and message error when create duplicate subject
    
    * feat: update auth api for the admin

--- src/apis/v1/admin/controller.ts
+++ src/apis/v1/admin/controller.ts
@@ -2,9 +2,11 @@ import { Response } from 'express';
 
 import * as userExamService from 'apis/v1/userExam/service';
 import * as documentService from 'apis/v1/documents/service';
+import * as authService from 'apis/v1/auth/service';
 import fmt from 'utils/formatter';
 import RequestWithUser from 'utils/rest/request';
 import { DocumentFilter } from '../documents/dto/DocumentsDto';
+import { LoginDto } from '../auth/dto/LoginDto';
 
 export const getAllUserExams = async (req: RequestWithUser, res: Response) => {
   const result = await userExamService.getAllUserExams();
@@ -14,5 +16,14 @@ export const getAllUserExams = async (req: RequestWithUser, res: Response) => {
 export const getDocumentsByAdmin = async (req: RequestWithUser, res: Response) => {
   const query: DocumentFilter = req.query;
   const result = await documentService.getDocumentsByAdmin(query);
+  res.send(fmt.formatResponse(result, Date.now() - req.startTime, 'OK'));
+};
+
+export const adminLogin = async (req: RequestWithUser, res: Response) => {
+  const input = req.headers['google-token'];
+  const checker = Array.isArray(input);
+  const token: LoginDto = checker ? { googleToken: input[0] } : { googleToken: input };
+
+  const result = await authService.adminLogin(token);
   res.send(fmt.formatResponse(result, Date.now() - req.startTime, 'OK'));
 };
--- src/apis/v1/admin/index.ts
+++ src/apis/v1/admin/index.ts
@@ -7,5 +7,6 @@ const router = Router();
 
 router.get('/user-exams', authMiddleware, adminMiddleware, asyncRouteHandler(controller.getAllUserExams));
 router.get('/documents', authMiddleware, adminMiddleware, asyncRouteHandler(controller.getDocumentsByAdmin));
+router.post('/login', asyncRouteHandler(controller.adminLogin));
 
 export default router;
